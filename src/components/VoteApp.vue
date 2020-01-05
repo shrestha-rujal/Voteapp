3e85ada3f0daeea7709ee69cb60f39197eb1fb90
Rujal Shrestha <rujal4shrz@gmail.com>
2020-01-05 23:13:53 +0545

    static ui for each vote component designed

---
+++ src/components/VoteApp.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="h-full container flex flex-col items-center">
+    <span class="text-gray-800 text-3xl py-10 font-semibold">UpVote!</span>
+    <div class="w-full flex flex-col items-center px-64">
+      <card>
+        <template v-slot:image>
+          <div class="bg-yellow-500 w-full h-full flex items-center justify-center">
+            <box-icon
+              size="40px"
+              color="white"
+              name='photo-album'>
+            </box-icon>
+          </div>
+        </template>
+        <template v-slot:title>
+          Yellow Pale
+        </template>
+        <template v-slot:rank>
+          1
+        </template>
+        <template v-slot:body>
+          On-demand sand castle construction expertise.
+        </template>
+        <template v-slot:vote-count>
+          10
+        </template>
+      </card>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './common/Card.vue';
+
+export default {
+  name: 'VoteApp',
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      message: 'hello',
+    };
+  },
+};
+</script>
---
+++ src/components/common/Card.vue
@@ -0,0 +1,69 @@
+<template>
+  <div
+    class="
+      border
+      border-gray-300
+      rounded
+      py-4
+      px-5
+      m-3
+      w-full
+      h-32
+      flex
+      items-center
+  ">
+    <div
+      class="rounded-sm w-20 h-20 flex items-center justify-center
+        border border-gray-300 rounded-sm"
+    >
+      <slot
+        name="image"
+      >
+        <box-icon
+          size="40px"
+          color="gray"
+          name='photo-album'>
+        </box-icon>
+      </slot>
+    </div>
+    <div class="flex-1 mx-5 flex flex-col">
+      <div>
+        <span class="text-blue-700 font-semibold mr-2">
+          <slot name="title">Card Heading</slot>
+        </span>
+        <span
+          class="bg-gray-300 py-1 px-2 text-xs text-gray-800 rounded-sm"
+        >#
+          <slot name="rank">0</slot>
+        </span>
+      </div>
+      <div class="py-2 text-sm flex-1 text-gray-800">
+        <slot name="body">Card Body</slot>
+      </div>
+      <div class="text-xs tracking-wide text-gray-600 flex items-center mt-2"
+      >
+        <span class="mr-1">Submitted By: </span>
+        <slot name="voter">
+          <box-icon
+            color="gray"
+            name='user-circle'>
+          </box-icon>
+        </slot>
+      </div>
+    </div>
+    <div class="h-full flex">
+      <box-icon name='chevron-up' ></box-icon>
+      <div
+        class="font-semibold text-blue-600 tracking-widest"
+      >
+        <slot name="vote-count">0</slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+};
+</script>
